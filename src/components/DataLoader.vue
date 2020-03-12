a5e04f162e9368c44cc142faced16cbd7f5b956b
Fog314 <navimed7@gmail.com>
2020-03-12 13:51:55 +0300

    Commit

--- src/components/DataLoader.vue
+++ src/components/DataLoader.vue
@@ -1,20 +1,29 @@
 <template>
-  <div class="hello">
-      <input v-if='isLoaded' type='submit' @click=load>
-            <div v-bind:style="{height:'304px'}" v-for="value in content" :key="value.id">
-              <div v-bind:style="{height:'50px', position: 'relative', width: '320px', margin: 'auto'}">
-                <div v-bind:style="{background:'url('+value.user.profile_image.small+') no-repeat', width:'30px', height: '30px', borderRadius: '50%', display: 'inline-block', position: 'absolute', left: '10px', top:'10px'}"></div>
-                <a :href=value.user.links.html><div class="info"><p v-bind:style="{margin:'0px'}">{{value.user.name}}</p><p v-if="value.user.twitter_username!=null" v-bind:style="{ color: 'gray',fontWeight:'normal', margin:'0px'}">@{{value.user.twitter_username}}</p></div></a>
-              </div>
-                <div v-bind:style="{width: '320px', height: '230px',background:'url('+value.urls.small+') no-repeat', backgroundPosition: '50%', margin: 'auto'}"></div>
-                <div v-bind:style="{textAlign: 'right',width: '320px', margin:'auto', fontSize: '90%', color:'gray', padding: '10px 10px 0px 0px'}">{{value.likes}}♥</div>
-            </div>
+<main>
+<div v-bind:style="{maxWidth: '660px', margin: 'auto'}">
+  <div class="grid" v-if='isLoaded==true'>
+        <div class="wrap" v-for="value in content" :key="value.id">
+          <div class="infoHeader">
+            <div v-bind:style="{background:'url('+value.user.profile_image.small+') no-repeat', width:'30px', height: '30px', borderRadius: '50%', display: 'inline-block', position: 'absolute', left: '10px', top:'10px'}"></div>
+            <div class="info"><a :href="value.user.links.html" target="_blank"><p v-bind:style="{margin:'0px'}">{{value.user.name}}</p></a><a v-if="value.user.twitter_username!=null"  :href="'http://twitter.com/' + value.user.twitter_username" target="_blank"><p v-bind:style="{ color: 'gray',fontWeight:'normal', margin:'0px'}">@{{value.user.twitter_username}}</p></a></div>
+          </div>
+            <div class="picture" v-bind:style="{background:'url('+value.urls.regular+') no-repeat'}"></div>
+            <div class="like">{{value.likes}}♥</div>
+        </div>
   </div>
+  <div v-else class="lds-dual-ring"></div>
+</div>
+  <div v-if='isLoaded==true' class="footer">
+      <button >
+        {{p}}
+      </button>
+  </div>
+</main>
 </template>
 
 <script>
-
 import Unsplash, { toJson } from "unsplash-js";
+// import _ from 'lodash';
 
 const unsplash = new Unsplash({
   accessKey: "p4bxahnq0buGn6UMZU7RxBMi7i4zFSYEa3uyBwkys0k",
@@ -32,37 +41,63 @@ export default {
   },
   data:function() {
       return{
-          url: 'http://vk.com',
-        //   url: {{value.user.profile_image.small}},
-    isLoaded: true,
-    object: {
-      title: 'How to do lists in Vue',
-      author: 'Jane Doe',
-      publishedAt: '2016-04-10'
-    }
+        isLoaded: false,
+        dataContent: [],
+        totalPhotos: 0,
+        perPage: 10,
+        currentPage: 1
       }
   },
-  methods:{
-    beforeMount(){
-      console.log('mounted');
-    },
-    mounted(){
+    mounted: async function() {
             console.log('mounted');
-            unsplash.photos.listPhotos(27,10, "latest")
+            await unsplash.photos.listPhotos(this.currentPage,this.perPage, "latest")
             .then(toJson)
             .then(json => {
+              this.to
               console.log(json);
               this.content = json;
+              console.log(toJson.headers.get('x-total'));
+              this.totalPhotos = parseInt(toJson.headers.get('x-total'));
+              console.log(typeof(this.dataContent));
+              this.isLoaded = true;
     });
-    this.isLoaded = false;
     }
-  },
 };
 
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang='scss' scoped>
+.lds-dual-ring {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+  display: inline-block;
+  width: 80px;
+  height: 80px;
+}
+.lds-dual-ring:after {
+  content: " ";
+  display: block;
+  width: 64px;
+  height: 64px;
+  margin: 8px;
+  border-radius: 50%;
+  border: 6px solid #fff;
+  border-color: rgb(0, 4, 255) transparent rgb(0, 4, 255) transparent;
+  animation: lds-dual-ring 1.2s linear infinite;
+}
+@keyframes lds-dual-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+
 h3 {
   margin: 40px 0 0;
 }
@@ -75,7 +110,21 @@ li {
   margin: 0 10px;
 }
 a {
-  color: #42b983;
+  color: black;
+  text-decoration: none;
+}
+
+.infoHeader{
+  height: 50px;
+  position: relative;
+  width: 320px; 
+  margin: auto;
+  @media(min-width: 480px){
+    width: 440px;
+  }
+  @media(min-width: 768px){
+    width: 320px;
+  }
 }
 
 .info{
@@ -86,5 +135,65 @@ a {
   left: 50px;
   top:10px;
   text-align: left;
+}
+
+.picture{
+  width: 320px;
+  height: 230px;
+  background-position: 50%;
+  background-size: cover!important;
+  margin: auto;
+  @media(min-width: 480px){
+    width: 440px;
+    height: 306px;
+  }
+
+  @media(min-width: 768px){
+    width: 320px;
+    height: 230px;
+  }
+}
+
+.like{
+  text-align: right;
+  width: 320px;
+  margin: auto; 
+  font-size: 90%;
+  color: gray;
+  padding: 10px 10px 0px 0px;
+  @media(min-width: 480px){
+    width: 400px;
+  }
+  @media(min-width: 768px){
+    width: 320px;
+  }
+}
+
+.wrap{
+  height: 304px;
+
+  @media(min-width: 480px){
+    height: 380px;
+  }
+  @media(min-width: 768px){
+    height: 312px;
+  }
+}
+
+.footer{
+  position: fixed;
+  top: 100%;
+  transform: translateY(-100%);
+  height: 60px;
+  width: 100%;
+  background: red;
+}
+
+.grid{
+  display: grid;
+  grid-template-columns: 1fr;
+  @media(min-width: 768px){
+    grid-template-columns: 1fr 1fr;
+  }
 }
 </style>
